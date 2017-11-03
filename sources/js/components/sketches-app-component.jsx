ef0d87cad3743e95bdd949791207ba546a917428
Mew-www <mew.proxy@hotmail.com>
2017-11-03 15:52:57 +0200

    Initial commit, only appcomponent with placeholder content

---
+++ sources/js/components/sketches-app-component.jsx
@@ -0,0 +1,19 @@
+import React, {Component} from "react";
+
+export class SketchesAppComponent extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className="SketchesApp">
+        <div className="SketchesApp__editor-container">
+          ♪♫♪♫♪♫♪♫<br/>♪♫♪♫♪♫♪♫
+        </div>
+        <div className="SketchesApp__menu-container">
+          ♪♫♪♫♪♫♪♫<br/>♪♫♪♫♪♫♪♫
+        </div>
+      </div>
+    );
+  }
+}
